d6bca965b3868be54a49bacc96b85a66897944ed
BuddhadevBauna <baunabuddhadev2001@gmail.com>
2024-05-24 23:14:57 +0530

    handle issue of cors policy in server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
+import 'dotenv/config';
 import express from "express";
 import authRoute from "./router/auth-router.js";
 import contactRoute from "./router/contact-router.js";
 import connectDB from "./utils/db.js";
 import errorMiddleware from "./middleware/error-middleware.js";
+import cors from "cors";
+
 
 const app = express();
 
 //middleware
+//handaling cors poicy issue
+//(that create when client request to server in differnt route(differnt route of client and server))
+var corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+}
+app.use(cors(corsOptions));
+//for use request body data as json
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
