ded9d21d00429bc5fe5ff814c10dc541317c21ca
BuddhadevBauna <baunabuddhadev2001@gmail.com>
2024-05-27 18:07:37 +0530

    added middleware to validate user is ligged in or not then show all details of user

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,9 +1,10 @@
 import express from "express";
 import adminController from "../controllers/admin-controller.js";
+import authMiddleware from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
-router.route('/users').get(adminController.getAllUsers);
-router.route('/contacts').get(adminController.getAllContacts);
+router.route('/users').get(authMiddleware, adminController.getAllUsers);
+router.route('/contacts').get(authMiddleware, adminController.getAllContacts);
 
 export default router;
